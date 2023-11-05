cca0e2e1007af743fbf80e907a1bac307530ad2f
qwimik <qwimik@gmail.com>
2023-11-05 16:40:56 +0200

    test commit 2

---
+++ app/design/frontend/Rozhnenko/default/Magento_Theme/requirejs-config.js
@@ -0,0 +1,9 @@
+const config = {
+    map: {
+        "*": {
+            tab1: "Magento_Theme/js/tab1",
+            tabs: "Magento_Theme/js/tabs",
+            btn: "Magento_Theme/js/btn",
+        },
+    },
+};
